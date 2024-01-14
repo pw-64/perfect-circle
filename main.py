2c040a0252bb3c9bae0a5eeea3cd77fafc547ccc
pw-64 <64085841+pw-64@users.noreply.github.com>
2024-01-14 23:17:44 +0000

    Update main.py
    added comments
    compacted and optimised code

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 # https://neal.fun/perfect-circle/
 
+# imports
 import pyautogui as pag
 from math import cos, sin, pi
 from time import sleep
 
+# config
 origin_x = 1137.5
 origin_y = 539.5
 r = 420
+smoothness = 6 # how many points around the circle to move the cursor to. higher = smoother but takes longer, so might run out of time and fail
 
+# generate coordinates to move to
 x_coordinates = []
 y_coordinates = []
 for angle in range(0, 360):
-    if angle % 6 == 0:
+    if angle % smoothness == 0:
         x_coordinates.append(origin_x + (r * cos(2 * pi * angle / 360)))
         y_coordinates.append(origin_y + (r * sin(2 * pi * angle / 360)))
 
+# draw the circle
 sleep(1)
-pag.moveTo(
-    origin_x + (r * cos(0)),
-    origin_y + (r * sin(0))
-)
+pag.moveTo(origin_x + r, origin_y)
 sleep(0.1)
 pag.mouseDown()
 sleep(0.1)
-
-for i in range(len(x_coordinates)):
-    pag.moveTo(x_coordinates[i], y_coordinates[i])
-
+for i in range(len(x_coordinates)): pag.moveTo(x_coordinates[i], y_coordinates[i])
 pag.mouseUp()
